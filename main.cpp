4e1008295b1c34ac1b019803906f9f13061720ce
SiddhartaDutta <siddhartadutta99@gmail.com>
2023-12-02 20:53:37 -0800

    Basic LED operations

---
+++ main.cpp
@@ -0,0 +1,114 @@
+// Final Project
+// C++ code
+//
+
+const char LEDPins[5] = {'A5', 'A4', 'A3', 'A2', 'A1'};
+
+enum DSTATE {INIT, DG1, DG2, DG3, DG4, DG5, RG} driveState = INIT;
+enum ALARM {OFF, ON} maxRPM = OFF;
+char shiftUpBtn = 0, shiftDownBtn = 0, ledCount = 0, throttle = 0, gear = 0, gearRec = 0;
+
+void onLEDs(){
+  for(char pin : LEDPins){
+    digitalWrite(pin, HIGH);
+  }
+}
+
+void offLEDs(){
+  for(char pin : LEDPins){
+    digitalWrite(pin, LOW);
+  }
+}
+
+void tick(void) {
+  
+  // transitions
+  switch (maxRPM){
+    case OFF:
+      if(!(throttle % 100)){
+        maxRPM = ON;
+        gearRec = gear;
+      }
+    break;
+
+    case ON:
+      if(gearRec != gear){
+        maxRPM = OFF;
+      }
+  }
+
+  switch (driveState) {
+    case INIT:
+      if(shiftUpBtn){
+        driveState = DG1;
+        gear++;
+      }
+
+      if(shiftDownBtn){
+        driveState = RG;
+        gear--;
+      }
+    break;
+    
+    case DG1:
+      if(shiftUpBtn){
+        driveState = DG2;
+        gear++;
+      }
+
+      if(shiftDownBtn){
+        driveState = INIT;
+        gear--;
+      }
+    break;
+
+    case DG2:
+      if(shiftUpBtn){
+        driveState = DG3;
+        gear++;
+      }
+
+      if(shiftDownBtn){
+        driveState = DG1;
+        gear--;
+      }
+    break;
+
+    case DG3:
+      if(shiftDownBtn){
+        driveState = DG2;
+        gear--;
+      }
+    break;
+
+    case RG:
+      if(shiftUpBtn){
+        driveState = INIT;
+        gear++;
+      }
+    break;
+  }
+  
+  // actions
+  switch (driveState) {
+    case INIT:
+    break;
+  }
+}
+
+void setup(){
+  
+  // LED Setup
+  for(char pin : LEDPins){
+    pinMode(pin, OUTPUT);
+  }
+  
+  // Buzzer Setup
+
+}
+
+void loop(){
+
+  onLEDs();
+
+}
